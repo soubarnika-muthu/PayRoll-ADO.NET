8d9b4c1147136aa1ecebf34f76714906ea18584c
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-04 19:57:22 +0530

    [Soubarnika]Creating Payroll Database Using ADO.NET

---
+++ Emp_PayRoll_ADO.NET/PayRoll_Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Emp_PayRoll_ADO.NET
+{
+    class PayRoll_Service
+    {
+        /// <summary>
+        ///UC1- Creating the connection
+        /// </summary>
+        public static string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=Employee_Payroll_Database";
+        //creating the object for sql connection class
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
---
+++ Emp_PayRoll_ADO.NET/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_PayRoll_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of PayRoll service");
+        }
+    }
+}
